bbf8fc8692ff6f20e87de0d53a708c1914714e95
Amoswish <1572912653@qq.com>
2017-11-20 11:50:11 +0800

    init1

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="cno" DataSourceID="SqlDataSource2" EmptyDataText="没有可显示的数据记录。">
+            <Columns>
+                <asp:BoundField DataField="cno" HeaderText="cno" ReadOnly="True" SortExpression="cno" />
+                <asp:BoundField DataField="cname" HeaderText="cname" SortExpression="cname" />
+                <asp:BoundField DataField="cpno" HeaderText="cpno" SortExpression="cpno" />
+                <asp:BoundField DataField="credit" HeaderText="credit" SortExpression="credit" />
+                <asp:BoundField DataField="teacher" HeaderText="teacher" SortExpression="teacher" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="Data Source=LENOVO-PC\SQLEXPRESS;Initial Catalog=data1;Integrated Security=True" ProviderName="System.Data.SqlClient" SelectCommand="SELECT * FROM [course]"></asp:SqlDataSource>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+public partial class _Default : System.Web.UI.Page
+{
+    //连接字符串 
+    //"<%$ ConnectionStrings:data1ConnectionString1 %>" DeleteCommand="DELETE FROM [course] WHERE [cno] = @cno" InsertCommand="INSERT INTO [course] ([cno], [cname], [cpno], [credit], [teacher]) VALUES (@cno, @cname, @cpno, @credit, @teacher)" ProviderName="<%$ ConnectionStrings:data1ConnectionString1.ProviderName %>" SelectCommand="SELECT [cno], [cname], [cpno], [credit], [teacher] FROM [course]" UpdateCommand="UPDATE [course] SET [cname] = @cname, [cpno] = @cpno, [credit] = @credit, [teacher] = @teacher WHERE [cno] = @cno">
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string aconnectionstring =  "Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=data1;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(aconnectionstring);
+        string sql = "select * from course";
+        SqlDataReader reader;
+
+        try
+        {
+            SqlCommand test = new SqlCommand(sql,connection);
+            connection.Open();
+            reader = test.ExecuteReader();
+
+            while (reader.Read()) {
+                Response.Write(reader.GetString(0));
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch
+        {
+
+        }
+        finally {
+            connection.Close();
+        }
+                 Response.Write("sss");
+    }
+}
